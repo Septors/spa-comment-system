3265f6a0ead7dbd5bdb3e6eeebd3c40e3da84b8e
Savinov Stanislav <savinov.stas2001@gmail.com>
2025-06-25 18:42:02 +0300

    Update redis.js

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,9 +1,7 @@
 import Redis from "ioredis";
 
 const redisClient =  process.env.REDIS_URL
-  ? new Redis(process.env.REDIS_URL,{
-  maxRetriesPerRequest: null, 
-  enableReadyCheck: false})
+  ? new Redis(process.env.REDIS_URL)
   :new Redis({
   host: process.env.REDIS_HOST, 
   port: process.env.REDIS_PORT, 
