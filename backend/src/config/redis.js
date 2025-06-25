6796e4490cafeb800ab729fb29978d74eaed983d
Savinov Stanislav <savinov.stas2001@gmail.com>
2025-06-25 12:52:19 +0300

    Update redis.js

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -3,6 +3,8 @@ import Redis from "ioredis";
 const redisClient = new Redis({
   host: process.env.REDIS_HOST, 
   port: process.env.REDIS_PORT, 
+  maxRetriesPerRequest: null, 
+  enableReadyCheck: false,
 });
 
 redisClient.on("error", (err) => {
